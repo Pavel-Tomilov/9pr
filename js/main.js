ebc56723fd0518230646338cc9f9e89b97f1fac4
Pavel Tomilov <pasha.tomiloff@yandex.ru>
2024-08-21 11:41:01 +0300

    one commit

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 class Card {
   _open = false
   _success = false
-constructor(container, number, action) {
-this.card = document.createElement('div')
-this.card.classList.add('card')
-this.card.textContent = number
-this.number = number
-this.card.addEventListener('click', () => {
-  if (this.open == false && this.success == false) {
-    this.open = true
-    action(this)
-  } 
-  
-})
+  constructor(container, number, action) {
+    this.card = document.createElement('div')
+    this.card.classList.add('card')
+    this.card.textContent = number
+    this.number = number
+    this.card.addEventListener('click', () => {
+      if (this.open == false && this.success == false) {
+        this.open = true
+        action(this)
+      }
 
-container.append(this.card)
-}
+    })
 
-set open(value) {
-this._open = value 
-value ? this.card.classList.add('open') : this.card.classList.remove('open')
-}
-
-get open() {
-  return this._open
-}
-
-set success(value) {
-  this.__success = value 
-  value ? this.card.classList.add('success') : this.card.classList.remove('success')
+    container.append(this.card)
   }
-    
+
+  set open(value) {
+    this._open = value
+    value ? this.card.classList.add('open') : this.card.classList.remove('open')
+  }
+
+  get open() {
+    return this._open
+  }
+
+  set success(value) {
+    this._success = value
+    value ? this.card.classList.add('success') : this.card.classList.remove('success')
+  }
+
   get success() {
     return this._success
   }
@@ -40,37 +40,67 @@ set success(value) {
 
 
 
-let cardsCount = 10,
-cardsNumberArray = [],
-cardsArray = [],
-firstCard = null,
-secondCard = null
+
 
 function newGame(container, cardsCount) {
-  for(let i = 1; i <= cardsCount / 2; i++) {
+
+
+    let cardsNumberArray = [],
+    cardsArray = [],
+    firstCard = null,
+    secondCard = null
+
+  for (let i = 1; i <= cardsCount / 2; i++) {
     cardsNumberArray.push(i)
     cardsNumberArray.push(i)
   }
-  
+
   cardsNumberArray.sort(() => Math.random() - 0.5);
-  for(const cardNumber of cardsNumberArray) {
-    cardsArray.push(new Card(container,cardNumber, flip))
+  for (const cardNumber of cardsNumberArray) {
+    cardsArray.push(new Card(container, cardNumber, flip))
   }
 
   function flip(card) {
-    if(firstCard == null){
+
+    if (firstCard !== null && secondCard !== null) {
+      if (firstCard.number != secondCard.number) {
+        firstCard.open = false
+        secondCard.open = false
+        firstCard = null
+        secondCard = null
+      }
+    }
+
+    if (firstCard == null) {
       firstCard = card
     } else {
-      if(secondCard == null) {
+      if (secondCard == null) {
         secondCard = card
       }
     }
-    console.log(firstCard);
-    console.log(secondCard);
+
+    if (firstCard !== null && secondCard !== null) {
+      if (firstCard.number == secondCard.number) {
+        firstCard.success = true
+        secondCard.success = true
+        firstCard = null
+        secondCard = null
+      }
+    }
+    if (document.querySelectorAll('.card.success').length == cardsNumberArray.length) {
+      alert('Победа!')
+      container.innerHTML = ''
+      cardsNumberArray = []
+      cardsArray = []
+      firstCard = null
+      secondCard = null
+
+      newGame(container, cardsCount)
+    }
   }
-  console.log(cardsNumberArray)
+
 }
 
-newGame(document.getElementById('game'), cardsCount)
+newGame(document.getElementById('game'), 6)
 
 
